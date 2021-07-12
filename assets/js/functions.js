13d1685a12ccf808b338169cf8c0846e33baac7b
SamaMah <87234281+SamaMah@users.noreply.github.com>
2021-07-12 23:35:22 +0200

    change ui mobile

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,12 @@
-$('.toggled-nav-wrapper').on('click', function(e){
-  e.stopPropoagation()
-});
-$('.nav-icon-open-wrapper, .nav-icon-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
-  $('html').toggleClass('nav-open');
-});
+// Stop propogation when button is pressed
+$('nav-icon-open-wrapper').on('click', function stopnav(e){ e.stopPropagation() });
+// Opens nav when button is pressed
+$('.nav-icon-open-wrapper, .nav-body-overlay').on('click', function(){ $('html').toggleClass('nav-open'); });
+
+const navIcon = $('.nav-icon-open-wrapper');
+const nav = $('.nav-links');
+
+const navSlide = function(){
+  navIcon.on('click', function(){ $('html').toggleClass('nav-active'); });
+}
+navSlide();
