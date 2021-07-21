07a0c7f59f2e05a1c36fe820627eb388a72ac1be
SamaMah <87234281+SamaMah@users.noreply.github.com>
2021-07-22 00:28:08 +0200

    layout test

--- assets/js/sketchbook.js
+++
@@ -1,9 +0,0 @@
-
-var elemntsFromCollection =  document.getElementsByClassName("classnameHere");
-var array = arrayOfElements = Array.from(elemntsFromCollection);
-array.forEach(function(item) {
-  var li = document.createElement("li");
-  var text = document.createTextNode(item);
-  li.appendChild(text);
-  document.getElementById("myUl").appendChild(li);
-});
